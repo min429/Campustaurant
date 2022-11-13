bbee8718fcb640a65f8fcfae34fe7bb0f1a5e353
seungmin <lsm990429@gmail.com>
2022-11-13 19:25:17 +0900

    Glide라이브러리 추가, 오늘의 메뉴(ImageView) 구현

--- app/src/main/java/com/example/campustaurant/MainActivity.java
+++ app/src/main/java/com/example/campustaurant/MainActivity.java
@@ -1,35 +1,49 @@
 package com.example.campustaurant;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
     private FirebaseAuth mFirebaseAuth;
     FirebaseDatabase database;
-    //FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser(); // 회원가입된 유저 객체를 가져온다.
+    FirebaseStorage storage;
+    DatabaseReference ref;
+    StorageReference stRef;
+    ArrayList<String> foodArrayList;
     Button btnLogout;
-    Button btnMatch;
     Button btnRoom;
     Button btnEnter;
     EditText etFood;
     String inputFood;
     String stUserId;
+    ImageView ivFood;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,12 +52,13 @@ public class MainActivity extends AppCompatActivity {
 
         mFirebaseAuth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance();
-
+        storage = FirebaseStorage.getInstance();
+        foodArrayList = new ArrayList<>();
         btnLogout = findViewById(R.id.btn_logout);
-        btnMatch = findViewById(R.id.btn_match);
         btnRoom = findViewById(R.id.btn_room);
         btnEnter = findViewById(R.id.btn_enter);
         etFood = findViewById(R.id.et_food);
+        ivFood = findViewById(R.id.iv_foodimg);
         stUserId = getIntent().getStringExtra("email"); // intent를 호출한 LoginActivity에서 email이라는 이름으로 넘겨받은 값을 가져와서 저장
 
         btnEnter.setOnClickListener(new View.OnClickListener() {
@@ -75,6 +90,38 @@ public class MainActivity extends AppCompatActivity {
 
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        ref = database.getReference("Food"); // Food하위에서 데이터를 읽기 위해
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                HashMap<String,String> map = (HashMap<String,String>)dataSnapshot.getValue(); // 파이어베이스 DB는 Map형태로 저장되어있기 때문에 HashMap/Map으로 불러와야함
+                for(String stFood : map.keySet()){ // map객체의 key값 리스트에서 값을 하나씩 가져와서 stFood에 저장
+                    foodArrayList.add(stFood); // map객체의 key값 리스트에서 값을 하나씩 가져와서 stFood에 저장
+                }
+                Log.d(TAG, "keySet: "+foodArrayList+", one: "+foodArrayList.get(0));
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e("MainActivity", String.valueOf(databaseError.toException())); // 에러문 출력
+            }
+        });
+
+        stRef = storage.getReference();
+
+        Log.d(TAG, "stRef: "+stRef);
+
+        stRef.child("돈까스.jpg").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Glide.with(MainActivity.this).load(uri).into(ivFood);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
             }
         });
     }
